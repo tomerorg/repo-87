81331059dc73b4ea8b04e03a2ef942940414e30f
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-16 14:01:04 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="repo-87",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Ui service implementing SQLAlchemy and Pytest architecture",
+    keywords="repo-87, python",
+    url="",
+)
